773d0ee5447f001b0d28ae5e433d6d9025772705
Jack Herrington <jherr@pobox.com>
2018-12-12 14:27:26 -0800

    Crystals and Score Board

---
+++ src/components/ScoreBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="header"
+  >
+    <div
+      class="title"
+    >
+      Vuewel Drop
+    </div>
+    <div
+      class="score"
+    >
+      {{ score }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    score: {
+      get() {
+        return this.$store.state.score;
+      },
+      set() {
+      },
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.header {
+  font-family: 'Nova Mono', sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  background: lightblue;
+  border-bottom: 2px solid darkblue;
+  margin-bottom: 5px;
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+}
+
+.title {
+  color:#666;
+  margin: 3px 10px;
+  padding: 2px;
+}
+
+.score {
+  background: white;
+  color: black;
+  text-align: right;
+  border: 1px solid blue;
+  margin: 3px 5px;
+  padding: 2px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import Crystal from '@/components/Crystal.vue';
+import ScoreBar from '@/components/ScoreBar.vue';
 
 Vue.config.productionTip = false;
+
+Vue.component('crystal', Crystal);
+Vue.component('score-bar', ScoreBar);
 
 new Vue({
   store,
